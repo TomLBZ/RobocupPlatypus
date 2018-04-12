29e7cf094c663a5ed83bcf5fe6a0d645ab42787e
jeanmah <jeanmah.828@gmail.com>
2018-04-12 22:53:59 +0800

    Add files via upload
    Test code for the servo
    Changed the AD0 pin for the gyro
    Added Servo Code to the main program

---
+++ ArduinoProMicroRelay/ArduinoProMicroRelay.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+#include <Wire.h>
+
+Servo servo;
+
+const int Servopin = 3;
+const int Servo0 = 4;
+const int Servo180 = 5;
+
+const int downAngle = 0;
+const int depositAngle = 180;
+const int holdAngle = 120;
+
+void setup() {
+  Serial.begin(115200);
+  servo.attach(3);
+  pinMode(Servo0, INPUT);
+  pinMode(Servo180, INPUT);
+}
+
+void loop() {
+  if(digitalRead(Servo0) == HIGH && digitalRead(Servo180) == LOW){
+    servo.write(downAngle);
+    delay(1000);
+    }
+  if(digitalRead(Servo0) == LOW && digitalRead(Servo180) == HIGH){
+    servo.write(depositAngle);
+    delay(1000);
+    }
+  if(digitalRead(Servo0) == LOW && digitalRead(Servo180) == LOW){
+    servo.write(holdAngle);
+    delay(1000);
+    }
+}
