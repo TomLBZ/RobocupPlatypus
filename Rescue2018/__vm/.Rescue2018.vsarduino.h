b251a9c40b61f87360b072d954929a2320e46339
李博钊 <libozhao142857@hotmail.com>
2018-04-22 14:27:42 +0800

    22AprFinalRescue

--- Rescue2018/__vm/.Rescue2018.vsarduino.h
+++ Rescue2018/__vm/.Rescue2018.vsarduino.h
@@ -68,7 +68,8 @@ typedef void *__builtin_va_list;
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
 
-typedef unsigned char uint8_t;
+//typedef unsigned char uint8_t;
+//typedef unsigned int uint8_t;
 
 #define pgm_read_byte(address_short) uint8_t() 
 #define pgm_read_word(address_short) uint16_t() 
